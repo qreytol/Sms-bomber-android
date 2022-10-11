9d6b644569c058725f4b60256af1df1f21da5ab8
Qreytol <68874501+qreytol@users.noreply.github.com>
2022-10-11 03:30:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,37 @@
-from kivy.uix.textinput import TextInput
-from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix.floatlayout import FloatLayout
+from kivymd.app import MDApp
+from kivymd.uix.screen import MDScreen
+from kivy.lang import Builder
 import bomber
 from get_info.get_information import get_information
 
-
-class MainTest(FloatLayout):
-    def press(self):
-        self.get_phone = self.ids.phone.text
-        get_phone = self.get_phone
-        if '380' not in get_phone:
-            print('ЧООООООООООО')
-            self.ids.start_bomb.text = 'Не правильно введено номер!'
-        elif len(get_phone) == 12 and '380' in get_phone:
-            self.ids.start_bomb.text = 'Спам почався!'
-            phone_inf = get_information(get_phone)
+class MainTest(MDScreen):
+    a = Builder.load_file('first copy.kv')
+    def start_spam(self):
+        get_phone = self.ids.enter_phone.text
+        get_minutes = self.ids.minutes.text
+        
+        if '380' not in get_phone or get_phone == '':
+            self.ids.start_spam_info.text = 'Не правильно введено номер'
+        
+        elif get_minutes == '':
+            self.ids.start_spam_info.text = 'Введи хвилини'
             
-            self.ids.operator.text = f'Оператор: {phone_inf[0]}'
-            self.ids.country.text = f'Країна: {phone_inf[1]}'
-            self.ids.geo.text = f'Приблизна геолокація: {phone_inf[2][0]}'
+        elif int(get_minutes) > 10:
+            self.ids.start_spam_info.text = 'Воу-воу куда розігнався тигр?'
             
-            bomber.start(get_phone)
         else:
-            print('ПОМИЛКА')
+            get_inf = get_information(get_phone)
+            self.ids.start_spam_info.text = 'Спам почався'
+            
+            self.ids.operator.text = f'Оператор: {get_inf[0]}'
+            self.ids.country.text = f'Країна: {get_inf[1]}'
+            self.ids.geo.text = f'Приблизна геолокація: {get_inf[2][0]}'
         
+            bomber.start(get_phone,get_minutes)
         
-
-class FirstApp(App):
+class FirstApp(MDApp):
     def build(self):
+        self.theme_cls.theme_style = 'Dark'
         return MainTest()
 
 FirstApp().run()
